2c96d64e55a8fc81b9866c61f7d7ee51a6de7d2b
VT71 <56217276+VT71@users.noreply.github.com>
2023-07-26 20:58:30 +0300

    Navigation Bar Responsiveness added. Logo Font Size changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,36 @@ function Header() {
                 <Toolbar
                     disableGutters
                     sx={{
+                        boxSizing: 'border-box',
                         display: 'flex',
                         flexDirection: 'row',
                         alignItems: 'center',
                         justifyContent: 'space-between',
+                        '@media (max-width: 599px)': {
+                            margin: '0 16px',
+                        },
+                        '@media (min-width: 600px)': {
+                            margin: '0 32px',
+                        },
+                        '@media (min-width: 905px)': {
+                            margin: '0 auto',
+                            width: '840px',
+                        },
+                        '@media (min-width: 1240px)': {
+                            margin: '0 200px',
+                            width: 'auto',
+                        },
+                        '@media (min-width: 1440px)': {
+                            margin: '0 auto',
+                            width: '1100px',
+                        },
                     }}
                 >
-                    <Typography>{`VT-AIR`}</Typography>
+                    <Typography
+                        sx={{
+                            fontFamily: 'Michroma, sans-serif',
+                        }}
+                    >{`VT-AIR`}</Typography>
                     <Box
                         sx={{
                             display: 'flex',
